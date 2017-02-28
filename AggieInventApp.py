a94327a8bab8321dc810e79a489c4fa58e4e830d
kmtamu <kmr.manish@tamu.edu>
2017-02-28 15:21:43 -0600

    backend works now

--- AggieInventApp.py
+++ AggieInventApp.py
@@ -1,17 +1,36 @@
 from flask import Flask, jsonify
+from flask import render_template
+from flask import request
 
 from models.WatsonModel import fetchClassification
 
-app = Flask(__name__)
+import cgi
+form = cgi.FieldStorage()
+searchterm =  form.getvalue('searchbox')
+
+
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('index.html')
+
+    #return "Hello"
 
 @app.route('/fetch')
 def view():
+    print(searchterm)
     return fetchClassification()
+
+
+
+
+@app.route('/getfile',methods=['POST'])
+def addRegion():
+    print("I got it!")
+    print(request.form['myFile'])
+    return render_template('some.html', data=request.form['myFile'])
 
 
 
